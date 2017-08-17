6571e42b8f3a04d699726c1baffc40e1da073adc
Bryan Stitt <bryan@stitthappens.com>
2017-08-16 18:18:21 -0700

    only send output to one device

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -3,7 +3,6 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
-#include <SoftwareSerial.h>
 #include <elapsedMillis.h>
 
 // Use these with the audio adaptor board
@@ -11,10 +10,6 @@
 #define SDCARD_MOSI_PIN  7
 #define SDCARD_SCK_PIN   14
 #define VOLUME_KNOB      A2
-
-// TODO: maybe better to use SPI, but this is less to solder
-// TODO: we don't even use RX on this
-SoftwareSerial            mySerial(2, 3);  // RX, TX
 
 AudioInputI2S             i2s1;           //xy=139,91
 AudioOutputI2S            i2s2;           //xy=392,32
@@ -25,8 +20,8 @@ AudioControlSGTL5000      audioShield;    //xy=366,225
 
 elapsedMillis elapsedMs = 0;    // todo: do we care if this overflows?
 
-// An array to hold the 16 frequency bands
-const int maxLevels = 16;  // TODO: should this be a define?
+// An array to hold the frequency bands
+const int maxLevels = 8;  // TODO: should this be a define?
 float level[maxLevels];
 
 int numLevels = maxLevels;  // TODO: read this from the SD card
@@ -34,11 +29,13 @@ int numLevels = maxLevels;  // TODO: read this from the SD card
 unsigned long turnOffMsArray[maxLevels];
 unsigned long lastUpdate = 0;
 
-uint minOnMs = 184; // 118? 150? 200?  // the shortest amount of time to leave an output on. todo: set this based on some sort of bpm detection? reda from the SD card
+// the sequencer also has this minOnMs logi
+// set the sequencer value as low as looks good with your lights.
+// set the teensy value to the value that looks good with the type of music you are listening to
+uint minOnMs = 150; // 118? 150? 184? 200?  // the shortest amount of time to leave an output on. todo: set this based on some sort of bpm detection? read from the SD card? have a button to switch between common settings?
 
-// each output gets 8 levels assigned to it
-const int numOutputs = 2;  // TODO: should this be a define?
-byte outputs[numOutputs];
+// 1 byte one for the sequencer board
+unsigned char output = 0;
 
 // An array to hold an EMA of the 16 frequency bands
 // When the signal drops quickly, these are used to lower the on-screen level
@@ -50,7 +47,7 @@ float decay = 0.9997;  // TODO: read this from the SD card
 
 // TODO: I'm still not sure I like how minMin interacts with scaling the levels
 float minMinLevel = 0.23;  // todo: tune this. read this from the SD card. maybe connect to the pot
-float minMaxLevel = 0.50;  // this is the lowest magnitude that will return a 1.  todo: read this from the SD card
+float minMaxLevel = 0.60;  // this is the lowest magnitude that will return a 1.  todo: read this from the SD card
 
 float maxLevel = minMaxLevel;
 float minLevel = minMinLevel;
@@ -61,7 +58,6 @@ float scaleFactor = maxLevel - minLevel;
 void setup() {
   Serial.begin(115200);  // todo: tune this
   Serial1.begin(115200);  // todo: tune this
-  mySerial.begin(115200);  // todo: tune this
 
   // delay(200);
 
@@ -83,15 +79,16 @@ void setup() {
   audioShield.muteHeadphone(); // to avoid any clicks
   audioShield.inputSelect(AUDIO_INPUT_MIC);
   audioShield.volume(0.5);
-  audioShield.micGain(40);  // was 63, then 50  // 0-63 // TODO: tune this
+  audioShield.micGain(60);  // was 63, then 40  // 0-63 // TODO: tune this
 
   audioShield.audioPreProcessorEnable();  // todo: pre or post?
 
   // bass, mid_bass, midrange, mid_treble, treble
   // TODO: tune this. maybe read from SD card
   audioShield.eqSelect(GRAPHIC_EQUALIZER);
-  audioShield.eqBands(-0.80, -0.75, -0.50, 0.50, 0.80);  // todo: tune this
-  //audioShield.eqBands(0, 0, 0, 0, 0);  // todo: tune this
+  // audioShield.eqBands(-0.80, -0.75, -0.50, 0.50, 0.80);  // the great northern
+  // audioShield.eqBands(-0.5, -.2, 0, .2, .5);  // todo: tune this
+  audioShield.eqBands(-0.80, -0.10, 0, 0.10, 0.33);  // todo: tune this
 
   audioShield.unmuteHeadphone();  // for debugging
 }
@@ -113,7 +110,7 @@ void levelFromFFT() {
     case 0:
     case 1:
       // TODO: this doesn't look right on our line graph
-      level[0] =  fft1024.read(0, 511);  // TODO: tune this
+      level[0] =  fft1024.read(0, 465);  // TODO: tune this 465 = 20k
       break;
     case 2:
       level[0] =  fft1024.read(1, 10);  // TODO: tune this
@@ -153,51 +150,57 @@ void levelFromFFT() {
       level[4] =  fft1024.read(21, 41);  // TODO: tune this
       level[5] =  fft1024.read(42, 82);  // TODO: tune this
       level[6] =  fft1024.read(83, 186);  // TODO: tune this
-    case 8:
-      level[0] =  fft1024.read(1, 3);  // TODO: tune this
-      level[1] =  fft1024.read(4, 6);  // TODO: tune this
-      level[2] =  fft1024.read(7, 10);  // TODO: tune this
-      level[3] =  fft1024.read(11, 20);  // TODO: tune this
-      level[4] =  fft1024.read(21, 41);  // TODO: tune this
-      level[5] =  fft1024.read(42, 82);  // TODO: tune this
-      level[6] =  fft1024.read(83, 164);  // TODO: tune this
-      level[7] =  fft1024.read(165, 186);  // TODO: tune this
-    case 9:
-    case 10:
-    case 11:
-    case 12:
-    case 13:
-    case 14:
-    case 15:
-    case 16:
-    default:
-      // these bands are from pjrc. we should try to have the other ones match this growth rate
-      // TODO: maybe skip the top and bottom bins?
-      level[0] =  fft1024.read(0);  // TODO: skip this bin?
-      level[1] =  fft1024.read(1);
-      level[2] =  fft1024.read(2, 3);
-      level[3] =  fft1024.read(4, 6);
-      level[4] =  fft1024.read(7, 10);
-      level[5] =  fft1024.read(11, 15);
-      level[6] =  fft1024.read(16, 22);
-      level[7] =  fft1024.read(23, 32);
-      level[8] =  fft1024.read(33, 46);
-      level[9] =  fft1024.read(47, 66);
-      level[10] = fft1024.read(67, 93);
-      level[11] = fft1024.read(94, 131);
-      level[12] = fft1024.read(132, 184);
-      level[13] = fft1024.read(185, 257);
-      level[14] = fft1024.read(258, 359);
-      level[15] = fft1024.read(360, 465);   // 465 = 20k
       break;
+    default:
+    case 8:
+      // level[0] =  fft1024.read(1, 6);  // TODO: tune this
+      // level[1] =  fft1024.read(7, 22);  // TODO: tune this
+      // level[2] =  fft1024.read(23, 32);  // TODO: tune this
+      // level[3] =  fft1024.read(33, 46);  // TODO: tune this
+      // level[4] =  fft1024.read(47, 66);  // TODO: tune this
+      // level[5] =  fft1024.read(67, 93);  // TODO: tune this
+      // level[6] =  fft1024.read(94, 184);  // TODO: tune this
+      // level[7] =  fft1024.read(185, 465);  // 396 = 17k  // TODO: tune this
+      level[0] =  fft1024.read(1, 15);    // TODO: ignore bin 0?
+      level[1] =  fft1024.read(16, 32);
+      level[2] =  fft1024.read(33, 46);
+      level[3] =  fft1024.read(47, 66);
+      level[4] = fft1024.read(67, 93);
+      level[5] = fft1024.read(94, 131);
+      level[6] = fft1024.read(132, 184);
+      level[7] = fft1024.read(185, 419);
+      break;
+    // case 9:
+    // case 10:
+    // case 11:
+    // case 12:
+    // case 13:
+    // case 14:
+    // case 15:
+    // case 16:
+    //   // these bands are from pjrc. we should try to have the other ones match this growth rate
+    //   // TODO: maybe skip the top and bottom bins?
+    //   level[0] =  fft1024.read(0);  // TODO: skip this bin?
+    //   level[1] =  fft1024.read(1);
+    //   level[2] =  fft1024.read(2, 3);
+    //   level[3] =  fft1024.read(4, 6);
+    //   level[4] =  fft1024.read(7, 10);
+    //   level[5] =  fft1024.read(11, 15);
+    //   level[6] =  fft1024.read(16, 22);
+    //   level[7] =  fft1024.read(23, 32);
+    //   level[8] =  fft1024.read(33, 46);
+    //   level[9] =  fft1024.read(47, 66);
+    //   level[10] = fft1024.read(67, 93);
+    //   level[11] = fft1024.read(94, 131);
+    //   level[12] = fft1024.read(132, 184);
+    //   level[13] = fft1024.read(185, 257);
+    //   level[14] = fft1024.read(258, 359);
+    //   level[15] = fft1024.read(360, 465);   // 465 = 20k
+    //   break;
   }
 }
 
-// 2 bytes. one for each sequencer board
-unsigned short output = 0;
-
-// pointers let us easily get to one byte at a time
-unsigned char* output_pointer = (unsigned char*)&output;
+int numOn = 0;
 
 
 void loop() {
@@ -248,13 +251,14 @@ void loop() {
     scaleFactor = (maxLevel - minLevel) * 0.95;
 
     // output the levels
-    // todo: limit the number of lights we turn on?
+    numOn = 0;
     for (int i = 0; i < numLevels; i++) {
       // scale so that 1 is the loudest sound we've heard
       float scaledLevel = (smoothLevel[i] - minLevel) / scaleFactor;
 
-      // todo: i think this should use the volume knob
-      if (scaledLevel > 0) {  // TODO: tune this
+      // todo: limit the number of lights we turn on to the loudest 6 of 8 instead of the first 6
+      //       i think we might still turn on 7 here with our turnOffMsArray logic :(
+      if ((numOn < 6) and (scaledLevel > 0.04)) {   // TODO: tune this
         // debug print
         Serial.print(scaledLevel);
         Serial.print(" ");
@@ -264,12 +268,16 @@ void loop() {
 
         // make sure we stay on for a minimum amount of time
         turnOffMsArray[i] = elapsedMs + minOnMs;
+
+        numOn += 1;
       } else {
         if (elapsedMs < turnOffMsArray[i]) {
           // the output has not been on for long enough. leave it on.
 
           // debug print
           Serial.print(" *   ");
+
+          numOn += 1;
         } else {
           // the output has been on for at least minOnMs and is quiet now. turn it off
           bitClear(output, i);
@@ -277,9 +285,7 @@ void loop() {
           // debug print
           Serial.print(" -   ");
         }
-
       }
-
     }
 
     // debug print
@@ -287,19 +293,13 @@ void loop() {
     Serial.print(AudioMemoryUsageMax());
     Serial.print(" blocks | ");
 
-    // TODO: software serial is crashing
-    // send first byte to Serial1
-    Serial1.write(output_pointer[0]);
-
-    // second next byte to mySerial
-    mySerial.write(output_pointer[1]);
+    // send byte to Serial1
+    Serial1.write(output);
 
     // make sure that first byte finished writing
     Serial1.flush();
-    // mySerial doesn't have a flush method :(
 
     // decay maxLevel
-    // TODO: this needs lots of tuning
     maxLevel = max(minMaxLevel, maxLevel * decay);
 
     // TODO: do something to minLevel?
